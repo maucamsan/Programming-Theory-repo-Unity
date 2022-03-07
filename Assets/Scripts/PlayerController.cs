11d2d00c0668b3635440bc7fb19805c5633beb6d
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-03-07 15:36:19 -0500

    Add basic charater movement
    Inside the player controller script, a simple movement system was added
    using the default axis to detect movement and using translation rather
    than using physics.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    float x = 0.0f;
+    float y = 0.0f; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        
+        transform.Translate(new Vector2(x * speed * Time.deltaTime, y * speed * Time.deltaTime));
         
     }
 }
